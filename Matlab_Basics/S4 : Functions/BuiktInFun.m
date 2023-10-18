6a89ecbc658041ed6b445eb140d6a18c439c3082
SalmaHossam7 <146543420+SalmaHossam7@users.noreply.github.com>
2023-10-18 20:21:11 +0300

    Add files via upload

---
+++ Matlab_Basics/S4 : Functions/BuiktInFun.m
@@ -0,0 +1,14 @@
+%create vector of numbers
+V = [1 7 3 6 5 9];
+
+%find the maximum number 
+maximumv = max(V);
+disp(['The maximum number is: ', num2str(maximumv)]);
+
+%find the minimum number 
+minimumv = min(V);
+disp(['The minimum number is: ', num2str(minimumv)]);
+
+%calculate the sum of all numbers
+total = sum(V);
+disp(['The sum of numbers is: ', num2str(total)]);
